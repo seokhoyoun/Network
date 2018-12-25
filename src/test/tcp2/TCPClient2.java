e55d758240042dea130ceeb0c8574cea4a46ac86
Seokho Youn <axlover@hotmail.com>
2018-12-25 13:24:47 +0900

    서버 try with resources

--- src/test/tcp2/TCPClient2.java
+++ src/test/tcp2/TCPClient2.java
@@ -1,6 +1,9 @@
 package test.tcp2;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -9,18 +12,42 @@ import java.util.Scanner;
 public class TCPClient2 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int port = 7777;
-		try {
-			String serverIP = InetAddress.getLocalHost().getHostAddress();
-			Socket socket = new Socket(serverIP,port);
-			
-			
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		clientStart();
 	}
 
+	private static void clientStart() {
+		Scanner sc = new Scanner(System.in);
+		BufferedReader br = null;
+		PrintWriter pw = null;
+		try {
+			Socket socket = new Socket(InetAddress.getLocalHost().getHostAddress(),8888);
+			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			pw = new PrintWriter(socket.getOutputStream());
+			while(true) {
+				System.out.print("전송 할 메세지 : ");
+				String line = sc.nextLine();
+				pw.println(line);
+				pw.flush();
+				if(line.equals("quit")) {
+					socket.close();
+					br.close();
+					pw.close();
+					break;
+				}
+				System.out.println(br.readLine());
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+				pw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
 }
--- src/test/tcp2/TCPServer2.java
+++ src/test/tcp2/TCPServer2.java
@@ -12,32 +12,42 @@ public class TCPServer2 {
 	
 	
 	public static void main(String[] args) {
-		try {
+		
 			startServer();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		
 	}
 
-	public static void startServer() throws IOException {
+	public static void startServer() {
 		int port = 8888;
-		ServerSocket server = new ServerSocket(port);
-		Socket client = server.accept(); 
-		try(ServerSocket ss = new ServerSocket();
+		try(ServerSocket server = new ServerSocket(port)){
+			while(true) {
+				System.out.println("새 연결을 기다리는 중...");
+				onServer(server);
+			}
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static void onServer(ServerSocket server) {
+		try(Socket client = server.accept(); 
 				BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
 					PrintWriter pw = new PrintWriter(client.getOutputStream())){
 			while(true) {
-				System.out.println("클라이언트 메세지 수신을 기다리는 중...");
+				System.out.println(client.getInetAddress().getHostAddress()+"의 메세지 수신을 기다리는 중...");
 				String cmes = br.readLine();
 				if(cmes.equals("quit")) {
-					pw.println("연결이 끊어졌습니다.");
+					pw.println("서버와의 연결을 종료합니다.");
+					System.out.println(client.getInetAddress().getHostAddress()+" 연결이 종료되었습니다.");
 					client.close();
+					break;
 				}
 				System.out.println("받은 메세지 : "+cmes);
-				pw.println("서버 메세지 수신...");
+				pw.println("Server received successfully....");
 				pw.flush();
-			}
 			
+			}	
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
